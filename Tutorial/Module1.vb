4120fe900b384d3e4bb0903a603050c3a4c4d883
khareemnurulla <khareemnurulla@users.noreply.github.com>
2015-03-24 00:12:16 +0800

    Update Module1.vb

--- Tutorial/Module1.vb
+++ Tutorial/Module1.vb
@@ -2,14 +2,7 @@
     Public con As New ADODB.Connection
     Public rs As New ADODB.Recordset
     Public Sub Connect()
-        Try
-            Dim path As String = Application.StartupPath & "\dbTutorial.mdb"
-            con.Open("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & path)
-        Catch ex As Exception
-            MsgBox(ex.StackTrace)
-        Finally
-
-        End Try
-        
+        Dim path As String = Application.StartupPath & "\dbTutorial.mdb"
+        con.Open("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & path)
     End Sub
 End Module
